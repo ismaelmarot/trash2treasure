4a1dc0d2b9f71d11779da1f04d1d88889b5dca37
ismaelmarot <43220852+ismaelmarot@users.noreply.github.com>
2026-03-08 19:42:42 -0300

    add: AppRouter

--- frontend/src/router/AppRouter.tsx
+++ frontend/src/router/AppRouter.tsx
@@ -1,12 +1,24 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import { MapPage } from '../pages'
+import { MapPage } from '../pages/MapPage/MapPage'
+// import { ItemDetailPage } from '../pages/ItemDetailPage/ItemDetailPage'
+// import { SearchPage } from '../pages/SearchPage/SearchPage'
+// import { AddItemPage } from '../pages/AddItemPage/AddItemPage'
+// import { ActivityPage } from '../pages/ActivityPage/ActivityPage'
+// import { ProfilePage } from '../pages/ProfilePage/ProfilePage'
+// import { ClaimConfirmationPage } from '../pages/ClaimConfirmationPage/ClaimConfirmationPage'
 
 export const AppRouter = () => {
-    return (
-        <BrowserRouter>
-            <Routes>
-                <Route path='/' element={<MapPage />} />
-            </Routes>
-        </BrowserRouter>
-    )
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<MapPage />} />
+        {/* <Route path='/item/:id' element={<ItemDetailPage />} />
+        <Route path='/search' element={<SearchPage />} />
+        <Route path='/add' element={<AddItemPage />} />
+        <Route path='/activity' element={<ActivityPage />} />
+        <Route path='/profile' element={<ProfilePage />} />
+        <Route path='/claim/:id' element={<ClaimConfirmationPage />} /> */}
+      </Routes>
+    </BrowserRouter>
+  )
 }
