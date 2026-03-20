9e29f924228d744c9c22446a9b349cf34c5e260f
ismaelmarot <43220852+ismaelmarot@users.noreply.github.com>
2026-03-20 01:40:37 -0300

    fix: Configurar PWA correctamente con meta tags en index.html

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,11 @@
 import { RouterProvider } from 'react-router-dom'
 import { router } from './router'
 import { AuthProvider } from './context/AuthContext'
-import { PWASetup } from './components/PWASetup'
 
 
 function App() {
   return (
     <AuthProvider>
-      <PWASetup />
       <RouterProvider router={router} />
     </AuthProvider>
   )
--- frontend/src/components/PWASetup/index.ts
+++
@@ -1,1 +0,0 @@
-export { PWASetup } from './PWASetup';
