eb7ba7754fd1da056ae2a490fc72bdba5a946930
ismaelmarot <43220852+ismaelmarot@users.noreply.github.com>
2026-03-21 10:51:53 -0300

    fix: Border-radius 34px en botones de cards (Eliminar, Liberar, Reclamar)

--- frontend/src/screens/SearchScreen/SearchScreen.styles.tsx
+++ frontend/src/screens/SearchScreen/SearchScreen.styles.tsx
@@ -250,7 +250,7 @@ export const ClaimButton = styled.button`
     color: ${COLORS.allWhite};
     border: none;
     padding: 14px;
-    border-radius: 14px;
+    border-radius: 34px;
     font-size: 14px;
     font-weight: 600;
     cursor: pointer;
