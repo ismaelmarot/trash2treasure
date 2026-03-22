9eef549bb39ad995046e2080a8d007c190f08129
ismaelmarot <43220852+ismaelmarot@users.noreply.github.com>
2026-03-22 09:25:23 -0300

    feat: Configurar Google Analytics con ID G-FHM9Y93V4D

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,7 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import ReactGA from 'react-ga4'
 import App from './App'
 import './styles.css'
+
+// Inicializar Google Analytics
+ReactGA.initialize('G-FHM9Y93V4D')
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
