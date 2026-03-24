4f1ea4a9593792a5e74c4c38533bbfb24a99a209
ismaelmarot <43220852+ismaelmarot@users.noreply.github.com>
2026-03-24 18:52:01 -0300

    Add Sentry for error monitoring and session replay

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,8 +1,25 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import ReactGA from 'react-ga4'
+import * as Sentry from '@sentry/react'
 import App from './App'
 import './styles.css'
+
+// Inicializar Sentry
+Sentry.init({
+  dsn: "https://94d8daaf09f58514337-bedaee2b6ebfc@o4511101677600768.ingest.us.sentry.io/4511101683433472",
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    Sentry.replayIntegration({
+      maskAllText: false,
+      blockAllMedia: false,
+    }),
+  ],
+  tracesSampleRate: 1.0,
+  replaysSessionSampleRate: 0.1,
+  replaysOnErrorSampleRate: 1.0,
+  sendDefaultPii: true,
+})
 
 // Inicializar Google Analytics
 ReactGA.initialize('G-FHM9Y93V4D')
