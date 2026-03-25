d0f74d6de65372d6b9edde5cfd3fc1e85b1084b5
ismaelmarot <43220852+ismaelmarot@users.noreply.github.com>
2026-03-25 16:18:09 -0300

    Fix camera zoom - prevent pinch zoom on camera controls

--- frontend/src/screens/AddItemScreen/AddItemScreen.styles.tsx
+++ frontend/src/screens/AddItemScreen/AddItemScreen.styles.tsx
@@ -325,12 +325,14 @@ export const CameraViewWrapper = styled.div`
     position: relative;
     ${size('100%','100%')}
     max-width: 500px;
+    touch-action: none;
 `
 
 export const VideoFeed = styled.video`
     width: 100%;
     height: 100%;
     object-fit: cover;
+    touch-action: none;
 `
 
 export const CameraControls = styled.div`
